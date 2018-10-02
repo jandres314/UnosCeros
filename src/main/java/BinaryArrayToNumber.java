c1d97203c0d07707078bd714bc4a95eaef61c77e
carlacdu <ceacevedo@sura.com.co>
2018-10-01 21:05:21 -0500

    commit inicial con todo el proyecto Done

---
+++ src/main/java/BinaryArrayToNumber.java
@@ -0,0 +1,9 @@
+import java.util.List;
+
+public class BinaryArrayToNumber {
+
+    public static int convertirArrayBinarioAInt(List<Integer> binario) {
+        //TODO: Solucion del problema
+        return 0;
+    }
+}
---
+++ src/test/java/BinaryArrayToNumberTest.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+
+public class BinaryArrayToNumberTest {
+
+    @Test
+    public void convertirArrayBinarioAIntCuandoRecibe0001DebeRetornar1() throws Exception {
+        //Arrange
+        List binario = new ArrayList<>(Arrays.asList(0,0,0,1));
+        //Act
+        int resp = BinaryArrayToNumber.convertirArrayBinarioAInt(binario);
+        //Assert
+        assertEquals(1,resp);
+    }
+
+    @Test
+    public void convertirArrayBinarioAIntCuandoRecibe0001DebeRetornar15() throws Exception {
+        List binario = new ArrayList<>(Arrays.asList(1,1,1,1));
+
+        int resp = BinaryArrayToNumber.convertirArrayBinarioAInt(binario);
+
+        assertEquals(1, resp);
+    }
+
+}
